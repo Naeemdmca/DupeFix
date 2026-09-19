5c8d4443c7c013e176dd2a71e6dfc45809be21f4
Naeemdmca <khangg174@gmail.com>
2026-09-19 23:58:11 +0300

    gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java-library")
+    id("xyz.jpenilla.run-paper") version "3.1.0"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-releases")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.github.retrooper:packetevents-spigot:2.13.0")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.11")
+        jvmArgs("-Xms2G", "-Xmx2G")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
